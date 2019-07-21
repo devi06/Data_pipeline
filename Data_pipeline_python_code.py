24c7eb9d257fd2171d8e667152b594adf4f77fe5
devi06 <52215558+devi06@users.noreply.github.com>
2019-07-20 23:56:04 -0400

    Update Data_pipeline_python_code.py

--- Data_pipeline_python_code.py
+++ Data_pipeline_python_code.py
@@ -7,7 +7,7 @@ Created on Thu Jul 18 22:41:34 2019
 #pip install sqlalchemy
 import sqlalchemy as db
 import cx_Oracle
-engine = db.create_engine('oracle://USER10:USER10@oracle10.cgsyjtny2rf9.us-east-2.rds.amazonaws.com:1521/orcl')
+engine = db.create_engine('oracle://<username>:<pass>@oracle10.cgsyjtny2rf9.us-east-2.rds.amazonaws.com:1521/orcl')
 
 import pandas as pd
 tables_df = pd.read_sql_query("select TABLE_NAME from all_tables WHERE OWNER='OT'",engine.connect())
@@ -22,5 +22,5 @@ for table in tab:
     #import pyarrow as pd
     #df.to_csv('warehouses.csv',index=False)  
     import boto3
-    s3 = boto3.client('s3', aws_access_key_id='AKIA6ERB5MROQWZOIFSW', aws_secret_access_key='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')
+    s3 = boto3.client('s3', aws_access_key_id='<accessKey>', aws_secret_access_key='<secretkey>')
     s3.upload_file(table+'.csv', 'shubhanandya', table+'.csv')
